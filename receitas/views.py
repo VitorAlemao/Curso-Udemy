3780da7a2a8fbeddf48647ee736a225b483288a9
VitorAlemao <vitorhugobravo@gmail.com>
2022-09-14 11:03:58 -0300

    Função render e home.html

--- receitas/views.py
+++ receitas/views.py
@@ -1,11 +1,11 @@
 from django.http import HttpResponse
-# from django.shortcuts import render
+from django.shortcuts import render
 
 # Create your views here.
 
 
 def home(request):
-    return HttpResponse('Essa é a pagina inicial')
+    return render(request, 'home.html')
 
 
 def sobre(request):
