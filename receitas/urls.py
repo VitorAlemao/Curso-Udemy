011e7e64fa1b2a1c7d1342eff3fc8892d1b1baac
VitorAlemao <vitorhugobravo@gmail.com>
2022-09-14 11:13:37 -0300

    Removido contato e sobre

--- receitas/urls.py
+++ receitas/urls.py
@@ -1,9 +1,8 @@
 from django.urls import path
 
-from receitas.views import contato, home, sobre
+from receitas.views import home
 
 urlpatterns = [
     path('', home),  # Home
-    path('sobre/', sobre),  # Sobre
-    path('contato/', contato),  # Contato
+    
 ]
--- receitas/views.py
+++ receitas/views.py
@@ -1,16 +1,8 @@
-from django.http import HttpResponse
+
 from django.shortcuts import render
 
 # Create your views here.
 
 
 def home(request):
-    return render(request, 'home.html')
-
-
-def sobre(request):
-    return HttpResponse('Essa pagina é sobre minha vida')
-
-
-def contato(request):
-    return HttpResponse('Essa pagina é sobre meu contato')
+    return render(request, 'home.html', context={'nome': 'Vitor Hugo', })
