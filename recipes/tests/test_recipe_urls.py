85b1b3e69d9c960a79627cecea120f61e6392ecf
VitorAlemao <vitorhugobravo@gmail.com>
2022-10-20 09:59:40 -0300

    Separando os testes em um package chamado tests com __init__.py

---
+++ recipes/tests/test_recipe_urls.py
@@ -0,0 +1,16 @@
+from django.test import TestCase
+from django.urls import reverse
+
+
+class RecipeURLsTest(TestCase):
+    def test_recipe_home_url_is_correct(self):
+        url = reverse('recipes:home')
+        self.assertEqual(url, '/')
+
+    def test_recipe_category_url_is_correct(self):
+        url = reverse('recipes:category', kwargs={'category_id': 5})
+        self.assertEqual(url, '/recipes/category/5/')
+
+    def test_recipe_detail_url_is_correct(self):
+        url = reverse('recipes:recipe', kwargs={'id': 2})
+        self.assertEqual(url, '/recipes/2/')
--- recipes/tests/test_recipe_view.py
+++ recipes/tests/test_recipe_view.py
@@ -1,22 +1,7 @@
 
 from django.test import TestCase
 from django.urls import resolve, reverse
-
 from recipes import views
-
-
-class RecipeURLsTest(TestCase):
-    def test_recipe_home_url_is_correct(self):
-        url = reverse('recipes:home')
-        self.assertEqual(url, '/')
-
-    def test_recipe_category_url_is_correct(self):
-        url = reverse('recipes:category', kwargs={'category_id': 5})
-        self.assertEqual(url, '/recipes/category/5/')
-
-    def test_recipe_detail_url_is_correct(self):
-        url = reverse('recipes:recipe', kwargs={'id': 2})
-        self.assertEqual(url, '/recipes/2/')
 
 
 class RecipeViewTest(TestCase):
