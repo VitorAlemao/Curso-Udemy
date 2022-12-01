2cd9a94c994c9e4f84dc0e251630a5e34fa01b22
VitorAlemao <vitorhugobravo@gmail.com>
2022-12-01 17:59:22 -0300

    Reutilizando a função add_attr com add_placeholder

--- authors/forms.py
+++ authors/forms.py
@@ -8,16 +8,17 @@ def add_attr(field, attr_name, attr_new_val):
 
 
 def add_placeholder(field, placeholder_val):
-    field.widget.attrs['placeholder'] = placeholder_val
+    add_attr(field, 'placeholder', placeholder_val)
 
 
 class RegisterForm(forms.ModelForm):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         add_placeholder(self.fields['username'], 'Your username')
-        add_placeholder(self.fields['email'], 'Your email')
-        add_placeholder(self.fields['first_name'], 'Ex.: Jhon')
+        add_placeholder(self.fields['email'], 'Your e-mail')
+        add_placeholder(self.fields['first_name'], 'Ex.: John')
         add_placeholder(self.fields['last_name'], 'Ex.: Doe')
+        add_attr(self.fields['username'], 'css', 'a-css-class')
 
     password = forms.CharField(
         required=True,
@@ -29,18 +30,20 @@ class RegisterForm(forms.ModelForm):
         },
         help_text=(
             'Password must have at least one uppercase letter, '
-            'one loweracase letter and one number. The lenght should be '
+            'one lowercase letter and one number. The length should be '
             'at least 8 characters.'
         )
     )
     password2 = forms.CharField(
         required=True,
         widget=forms.PasswordInput(attrs={
-            'placeholder': 'Repeat your password here'
+            'placeholder': 'Repeat your password'
         }),
-        error_messages={
-            'required': 'Password must not be empty'
-        }
+        help_text=(
+            'Password must have at least one uppercase letter, '
+            'one lowercase letter and one number. The length should be '
+            'at least 8 characters.'
+        )
     )
 
     class Meta:
@@ -73,7 +76,7 @@ class RegisterForm(forms.ModelForm):
         widgets = {
             'first_name': forms.TextInput(attrs={
                 'placehorlder': 'Type your username here',
-                'class': 'input text-input outra-classe',
+                'class': 'input text-input',
             }),
             'password': forms.PasswordInput(attrs={
                 'placeholder': 'Type your password here'
