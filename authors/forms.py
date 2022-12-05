4334101a85067ad5f3e6bb9ea7d41b2cb3c09b11
VitorAlemao <vitorhugobravo@gmail.com>
2022-12-05 20:11:52 -0300

    Utilizando o atributo "validators" do próprio campo para validação de dados

--- authors/forms.py
+++ authors/forms.py
@@ -1,3 +1,5 @@
+import re
+
 from django import forms
 from django.contrib.auth.models import User
 from django.core.exceptions import ValidationError
@@ -10,6 +12,18 @@ def add_attr(field, attr_name, attr_new_val):
 
 def add_placeholder(field, placeholder_val):
     add_attr(field, 'placeholder', placeholder_val)
+
+
+def strong_password(password):
+    regex = re.compile(r'^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9]).{8,}$')
+
+    if not regex.match(password):
+        raise ValidationError((
+            'Password must have at least one uppercase letter, '
+            'one lowercase letter and one number. The length should be '
+            'at least 8 characters.'
+        ),
+            code='Invalid')
 
 
 class RegisterForm(forms.ModelForm):
@@ -33,7 +47,8 @@ class RegisterForm(forms.ModelForm):
             'Password must have at least one uppercase letter, '
             'one lowercase letter and one number. The length should be '
             'at least 8 characters.'
-        )
+        ),
+        validators=[strong_password]
     )
     password2 = forms.CharField(
         required=True,
@@ -110,11 +125,11 @@ class RegisterForm(forms.ModelForm):
 
         if password != password2:
             password_confirmation_error = ValidationError(
-                    'Password and password2 must be equal',
-                    code='invalid')
+                'Password and password2 must be equal',
+                code='invalid')
             raise ValidationError({
                 'password': password_confirmation_error,
                 'password2': [
                     password_confirmation_error
-                    ],
+                ],
             })
